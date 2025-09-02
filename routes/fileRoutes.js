e8da2f4c4f877dc8507230b7f25e827985f69a32
mahe1999mh <mahe1999mh@gmial.com>
2025-09-02 18:23:51 +0530

    added middleware

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -7,11 +7,12 @@ const {
   updateById,
   deleteById,
 } = require("../controllers/fileController");
+const {verifyToken} = require("../middleware/authMiddleware");
 
 router.post("/", createFilefolders);
-router.get("/", getAll);
-router.get("/:id", getById);
-router.put("/:id", updateById);
-router.delete("/:id", deleteById);
+router.get("/", verifyToken, getAll);
+router.get("/:id", verifyToken, getById);
+router.put("/:id", verifyToken, updateById);
+router.delete("/:id", verifyToken, deleteById);
 
 module.exports = router;
